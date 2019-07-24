MobilityNet/mobilitynet-analysis-scripts

commit 01834ad2955c4c875aae3a1eecbffaf30d748fca

Fix issues found while adding multi-modal shim sections
While creating our first multi-modal trip, found and fixed some minor issues
related to the added shim trips. Notable changes:
- we need to potentially add multiple shim trips while transitioning from a
  vehicle to a multi-occupancy vehicle (e.g. bus or train). In that case, there
  is a walking segment from the first vehicle to the second and a stopped
  segment when we are waiting to board the second.
- we cannot append the shim legs, we need to insert them into the appropriate
  place. Since we can return two shims, this gets more complicated
- changed the leg population to a functional paradigm to make this easier; the
  validation code now returns the filled section and we assemble a new section
  list that we assign to the original trip.

Also, since we now have so many sections, specify a ratio while creating the
branca figure so that the maps don't become too tiny.

spec_creation/autofill_eval_spec.py
@@ -43,7 +43,8 @@ def _fill_coords_from_id(loc):
         if loc["geometry"]["type"] == "Point":
             loc["geometry"]["coordinates"] = node_to_geojson_coords(loc["properties"]["osm_id"])
         elif loc["geometry"]["type"] == "Polygon":
-            loc["geometry"]["coordinates"] = get_coords_for_way(loc["properties"]["osm_id"])
+            # get coords for way returns a tuple of (nodes, points)
+            loc["geometry"]["coordinates"] = [[coords_swap(c) for c in get_coords_for_way(loc["properties"]["osm_id"])[1]]]
     else:
         assert "coordinates" in loc["geometry"],\
             "Location %s does not have either an osmid or specified set of coordinates"
@@ -151,7 +152,9 @@ def get_route_from_relation(t):
     return get_coords_for_relation(t["relation"],
         t["start_loc"]["properties"]["osm_id"], t["end_loc"]["properties"]["osm_id"])
 
-def validate_and_fill_leg(t):
+def validate_and_fill_leg(orig_leg):
+    # print(t)
+    t = copy.copy(orig_leg)
     t["type"] = "TRAVEL"
     start_coords = _fill_coords_from_id(t["start_loc"])
     end_coords = _fill_coords_from_id(t["end_loc"])
@@ -184,46 +187,68 @@ def validate_and_fill_leg(t):
             "coordinates": [coords_swap(rc) for rc in route_coords]
         }
     }
+    return t
 
 def get_hidden_access_transfer_walk_segments(prev_l, l):
+    # print("prev_l = %s, l = %s" % (prev_l, l))
     if prev_l is None and l["mode"] != "WALKING":
         # This is the first leg and is a vehicular trip,
         # need to add an access leg to represent the walk to where the
         # vehicle will be parked. This is unknown at spec creation time,
         # so we don't have any ground truth for it
-        return {
+        return [{
             "id": "walk_start",
             "type": "ACCESS",
             "mode": "WALKING",
             "name": "Walk from the building to your vehicle",
             "loc": l["start_loc"],
-        }
+        }]
 
     if l is None and prev_l["mode"] != "WALKING":
         # This is the first leg and is a vehicular trip,
         # need to add an access leg to represent the walk to where the
         # vehicle will be parked. This is unknown at spec creation time,
         # so we don't have any ground truth for it
-        return {
+        return [{
             "id": "walk_start",
             "type": "ACCESS",
             "mode": "WALKING",
             "name": "Walk from your vehicle to the building",
             "loc": prev_l["end_loc"]
-        }
+        }]
 
-    if prev_l["mode"] != "WALKING" and l["mode"] != "WALKING":
+    # The order of the checks is important because we want the STOPPED to come
+    # after the WALKING
+    ret_list = []
+
+    if prev_l is not None and l is not None and\
+        prev_l["mode"] != "WALKING" and l["mode"] != "WALKING":
         # transferring between vehicles, add a transit transfer
         # without a ground truthed trajectory
-        return {
-            "id": "tt_%d_%d" % (i-1, i),
+        # NOTE: unlike the first two cases, we are NOT returning here
+        # we will run the next check as well, because for most
+        # transit transfers, there will be both a transfer and a stop
+        ret_list.append({
+            "id": "tt_%s_%s" % (prev_l["mode"], l["mode"]),
             "type": "TRANSFER",
             "mode": "WALKING",
             "name": "Transfer between %s and %s at %s" %\
-                (prev_l["mode"], l["mode"], prev_l["end_loc"]["name"]),
-            "start_loc": prev_l["end_loc"],
-            "end_loc": l["start_loc"]
-        }
+                (prev_l["mode"], l["mode"], prev_l["end_loc"]["properties"]["name"]),
+            "loc": l["start_loc"]
+        })
+
+    if l is not None and "multiple_occupancy" in l and l["multiple_occupancy"] == True:
+        ret_list.append({
+            "id": "wait_for_%s" % (l["mode"]),
+            "type": "WAITING",
+            "mode": "STOPPED",
+            "name": "Wait for %s at %s" %\
+                (l["mode"], l["start_loc"]["properties"]["name"]),
+            "loc": l["start_loc"]
+        })
+
+    # return from the last two checks
+    return ret_list
 
 
 def validate_and_fill_eval_trips(curr_spec):
@@ -231,19 +256,25 @@ def validate_and_fill_eval_trips(curr_spec):
     eval_trips = modified_spec["evaluation_trips"]
     for t in eval_trips:
         if "legs" in t:
+            print("Filling multi-modal trip %s" % t["id"])
             prev_l = None
+            ret_leg_list = []
             for i, l in enumerate(t["legs"]):
+                print("Filling leg %s" % l["id"])
                 # Add in shim legs like the ones to walk to/from your vehicle
                 # or to transfer between transit modes
-                shim_leg = get_hidden_access_transfer_walk_segments(prev_l, l)
-                if shim_leg is not None:
-                    t["legs"].append(shim_leg)
-                validate_and_fill_leg(l)
+                shim_legs = get_hidden_access_transfer_walk_segments(prev_l, l)
+                print("Got shim legs %s, extending" % ([sl["id"] for sl in shim_legs]))
+                ret_leg_list.extend(shim_legs)
+                ret_leg_list.append(validate_and_fill_leg(l))
                 prev_l = l
             shim_leg = get_hidden_access_transfer_walk_segments(prev_l, None)
-            if shim_leg is not None:
-                t["legs"].append(shim_leg)
+            assert len(shim_leg) <= 1, "Last leg should not have a transfer shim"
+            print("Got shim legs %s, extending" % ([sl["id"] for sl in shim_leg]))
+            ret_leg_list.extend(shim_legs)
+            t["legs"] = ret_leg_list
         else:
+            print("Filling unimodal trip %s" % t["id"])
             # unimodal trip, let's add shims if necessary
             # the filled spec will always be multimodal
             # since the only true unimodal trip is walking
@@ -256,13 +287,14 @@ def validate_and_fill_eval_trips(curr_spec):
             t["name"] = unmod_trip["name"]
             t["legs"] = []
             before_shim_leg = get_hidden_access_transfer_walk_segments(None, unmod_trip)
-            if before_shim_leg is not None:
-                t["legs"].append(before_shim_leg)
-            t["legs"].append(unmod_trip)
-            validate_and_fill_leg(unmod_trip)
+            assert len(before_shim_leg) <= 1, "First leg should not have a transfer shim"
+            print("Got shim legs %s, extending" % ([sl["id"] for sl in before_shim_leg]))
+            t["legs"].extend(before_shim_leg)
+            t["legs"].append(validate_and_fill_leg(unmod_trip))
             after_shim_leg = get_hidden_access_transfer_walk_segments(unmod_trip, None)
-            if after_shim_leg is not None:
-                t["legs"].append(after_shim_leg)
+            assert len(after_shim_leg) <= 1, "Last leg should not have a transfer shim"
+            print("Got shim legs %s, extending" % ([sl["id"] for sl in after_shim_leg]))
+            t["legs"].extend(after_shim_leg)
 
     return modified_spec
 

spec_creation/Validate_spec_before_upload.ipynb
@@ -70,7 +70,7 @@
     "        assert len(loc[\"geometry\"][\"coordinates\"]) == 1,\\\n",
     "            \"Only simple polygons supported!\"\n",
     "        curr_latlng = [lonlat_swap(c) for c in loc[\"geometry\"][\"coordinates\"][0]]\n",
-    "        print(\"Returning polygon for %s\" % curr_latlng)\n",
+    "        # print(\"Returning polygon for %s\" % curr_latlng)\n",
     "        return folium.PolyLine(curr_latlng, color=disp_color, fill=disp_color,\n",
     "                  popup=\"%s\" % loc[\"properties\"][\"name\"])        "
    ]
@@ -88,7 +88,7 @@
    "metadata": {},
    "outputs": [],
    "source": [
-    "spec_to_validate = json.load(open(\"unimodal_roundtrip.filled.json\"))\n",
+    "spec_to_validate = json.load(open(\"car_scooter_brex_san_jose.filled.json\"))\n",
     "sensing_configs = json.load(open(\"sensing_regimes.all.specs.json\"))"
    ]
   },
@@ -226,7 +226,7 @@
     "            map_list.append(curr_map)\n",
     "\n",
     "rows = get_row_count(len(map_list), 2)\n",
-    "evaluation_maps = bre.Figure()\n",
+    "evaluation_maps = bre.Figure(ratio=\"{}%\".format((rows/2) * 100))\n",
     "for i, curr_map in enumerate(map_list):\n",
     "    evaluation_maps.add_subplot(rows, 2, i+1).add_child(curr_map)\n",
     "evaluation_maps"

commit db97ceabfcf492f88f2042dccbbabeb3c4464839

Bunch of fixes from filling in the UC Berkeley round trip
Fixes while creating UCB round trip (8ab4d8115362b376ea8ff88558370a0cd81d34c6)
Related viz fixes are dc0607322b256a8d2e6251512c6b92f9ad123c29 and fdf835df3349a15907875acc328675eb6cc76c72

Concretely:
- Convert the waypoint coords also to geojson
- allow `waypoint_coords` to be missing and to use OSRM as the default if `polyline` or `relation` are not present
- use shapely to convert start and end polygons to points to allow OSRM to work
- fix stupid bug with the last walk shim leg, introduced in 01834ad2955c4c875aae3a1eecbffaf30d748fca

spec_creation/autofill_eval_spec.py
@@ -8,6 +8,7 @@ import osmapi
 import re
 import polyline as pl
 import osrm as osrm
+import shapely.geometry as geo
 
 sensing_configs = json.load(open("sensing_regimes.all.specs.json"))
 
@@ -48,7 +49,7 @@ def _fill_coords_from_id(loc):
     else:
         assert "coordinates" in loc["geometry"],\
             "Location %s does not have either an osmid or specified set of coordinates"
-    return loc["geometry"]["coordinates"]
+    return loc
 
 def validate_and_fill_calibration_tests(curr_spec):
     modified_spec = copy.copy(curr_spec)
@@ -66,9 +67,18 @@ def get_route_from_osrm(t, start_coords, end_coords):
     if "route_waypoints" in t:
         waypoints = t["route_waypoints"]
         waypoint_coords = [node_to_geojson_coords(node_id) for node_id in waypoints]
-        t["waypoint_coords"] = waypoint_coords
+        t["waypoint_coords"] = {
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+                "type": "Polygon",
+                "coordinates": waypoint_coords
+            }
+        }
     elif "waypoint_coords" in t:
-        waypoint_coords = t["waypoint_coords"]
+        waypoint_coords = t["waypoint_coords"]["geometry"]["coordinates"]
+    else:
+        waypoint_coords = []
     logging.debug("waypoint_coords = %s..." % waypoint_coords[0:3])
     route_coords = get_route_coords(t["mode"],
         [start_coords] + waypoint_coords + [end_coords])
@@ -143,21 +153,23 @@ def get_coords_for_relation(rid, start_node, end_node):
 
 def get_route_from_relation(t):
     # get_coords_for_relation assumes that start and end are both nodes
-    assert "osm_id" in t["start_loc"]["properties"] and \
-        t["start_loc"]["geometry"]["type"] == "Point",\
-        "Start location is not an OSM node, cannot find relation from route"
-    assert "osm_id" in t["start_loc"]["properties"] and \
-        t["end_loc"]["geometry"]["type"] == "Point",\
-        "End location is not a node, cannot find relation from route"
-    return get_coords_for_relation(t["relation"],
-        t["start_loc"]["properties"]["osm_id"], t["end_loc"]["properties"]["osm_id"])
+    return get_coords_for_relation(t["relation"]["relation_id"],
+        t["relation"]["start_node"], t["relation"]["end_node"])
 
 def validate_and_fill_leg(orig_leg):
     # print(t)
     t = copy.copy(orig_leg)
     t["type"] = "TRAVEL"
-    start_coords = _fill_coords_from_id(t["start_loc"])
-    end_coords = _fill_coords_from_id(t["end_loc"])
+    # These are now almost certain to be polygons
+    # and probably user-drawn, not looked up from OSM
+    # so what we will get here is an geojson representation of a polygon
+    # TODO: Drop support for single point
+    start_polygon = _fill_coords_from_id(t["start_loc"])
+    end_polygon = _fill_coords_from_id(t["end_loc"])
+    print("Raw polygons: start = %s..., end = %s..." %
+        (start_polygon["geometry"]["coordinates"][0][0:3],
+         end_polygon["geometry"]["coordinates"][0][0:3]))
+
     # there are three possible ways in which users can specify routes
     # - waypoints from OSM, which we will map into coordinates and then
     # move to step 2
@@ -172,12 +184,18 @@ def validate_and_fill_leg(orig_leg):
     # integration (otp/routers/default/plan?)
     # But once people figure out the underlying call, they can copy-paste the
     # geometry into the spec.
-    if "route_waypoints" in t or "waypoint_coords" in t:
-        route_coords = get_route_from_osrm(t, start_coords, end_coords)
-    elif "polyline" in t:
+    if "polyline" in t:
         route_coords = get_route_from_polyline(t)
     elif "relation" in t:
         route_coords = get_route_from_relation(t)
+    else:
+        # We need to find a point within the polygon to pass to the routing engine
+        start_coords_shp = geo.Polygon(start_polygon["geometry"]["coordinates"][0]).representative_point()
+        start_coords = geo.mapping(start_coords_shp)["coordinates"]
+        end_coords_shp = geo.Polygon(end_polygon["geometry"]["coordinates"][0]).representative_point()
+        end_coords = geo.mapping(end_coords_shp)["coordinates"]
+        print("Representative_coords: start = %s, end = %s" % (start_coords, end_coords))
+        route_coords = get_route_from_osrm(t, start_coords, end_coords)
 
     t["route_coords"] = {
         "type": "Feature",
@@ -268,9 +286,9 @@ def validate_and_fill_eval_trips(curr_spec):
                 ret_leg_list.extend(shim_legs)
                 ret_leg_list.append(validate_and_fill_leg(l))
                 prev_l = l
-            shim_leg = get_hidden_access_transfer_walk_segments(prev_l, None)
-            assert len(shim_leg) <= 1, "Last leg should not have a transfer shim"
-            print("Got shim legs %s, extending" % ([sl["id"] for sl in shim_leg]))
+            shim_legs = get_hidden_access_transfer_walk_segments(prev_l, None)
+            assert len(shim_legs) <= 1, "Last leg should not have a transfer shim"
+            print("Got shim legs %s, extending" % ([sl["id"] for sl in shim_legs]))
             ret_leg_list.extend(shim_legs)
             t["legs"] = ret_leg_list
         else: