9d79a026c034d16c59d6002b7a373475816c91b0
K. Shankari <shankari@eecs.berkeley.edu>
2019-08-27 17:46:48 -0700

    Support timestamps in point markers
    If the linestring has associated timestamps as a property, display them in the
    popups as well

--- emeval/viz/phone_view.py
+++ emeval/viz/phone_view.py
@@ -192,12 +192,20 @@ def get_geojson_for_leg(sensed_section, color="red"):
     location_df = sensed_section["location_df"]
     lonlat_route_coords = list(zip(location_df.longitude, location_df.latitude))
     return gj.Feature(geometry=gj.LineString(lonlat_route_coords),
-        properties={"style": {"color": color}})
+        properties={"style": {"color": color}, "ts": list(location_df.ts)})
 
-def get_point_markers(linestring_gj, name="points", **kwargs):
+def get_point_markers(linestring_gj, name="points", tz="UTC", **kwargs):
     fg = folium.FeatureGroup(name)
+    if "properties" in linestring_gj and "ts" in linestring_gj["properties"]:
+        ts_list = linestring_gj["properties"]["ts"]
+    else:
+        ts_list = None
     for i, c in enumerate(linestring_gj["geometry"]["coordinates"]):
-        folium.CircleMarker(lonlat_swap(c), radius=5, popup="%d: index: %s" % (i, c), **kwargs).add_to(fg)
+        if ts_list is not None:
+            ts = ts_list[i]
+            folium.CircleMarker(lonlat_swap(c), radius=5, popup="%d: %s, %s" % (i, c, arrow.get(ts).to(tz).format("YYYY-MM-DD HH:mm:ss")), **kwargs).add_to(fg)
+        else:
+            folium.CircleMarker(lonlat_swap(c), radius=5, popup="%d: %s" % (i, c), **kwargs).add_to(fg)
     return fg
 
 def print_entry(e, metadata_field_list, data_field_list, tz):
