ec287768e442f413652e8988916c509a876bd7c1
singhish <ishratsingh00@gmail.com>
2021-03-16 18:42:21 -0400

    significant refactors + add fill transitions functionality

--- bin/dump_data_to_file.py
+++ bin/dump_data_to_file.py
@@ -4,9 +4,13 @@ import argparse
 import sys
 import json
 import arrow
+import copy
 
 
-DEFAULT_USER = "shankari@eecs.berkeley.edu"
+# instance variables for script
+SPEC_USER = "shankari@eecs.berkeley.edu"
+DATASTORE_URL = None
+SPEC_ID = None
 
 # modified SpecDetails class to keep track of spec details
 class Spec:
@@ -26,7 +30,7 @@ class Spec:
         self.phones = self.spec["phones"]
 
 
-def retrieve_data_from_server(datastore_url, key, user, start_ts, end_ts):
+def retrieve_data_from_server(key, user, start_ts, end_ts):
     post_body = {
         "user": user,
         "key_list": [key],
@@ -36,23 +40,74 @@ def retrieve_data_from_server(datastore_url, key, user, start_ts, end_ts):
 
     print(f"Retrieving data for: {post_body=}")
     try:
-        response = requests.post(f"{datastore_url}/datastreams/find_entries/timestamp", json=post_body)
+        response = requests.post(f"{DATASTORE_URL}/datastreams/find_entries/timestamp", json=post_body)
         print(f"{response=}")
         response.raise_for_status()
-        ret_list = response.json()["phone_data"]
+        data = response.json()["phone_data"]
     except Exception as e:
         print(f"Got {type(e).__name__}: {e}, retrying...")
         time.sleep(10)
-        response = requests.post(f"{datastore_url}/datastreams/find_entries/timestamp", json=post_body)
+        response = requests.post(f"{DATASTORE_URL}/datastreams/find_entries/timestamp", json=post_body)
         print(f"{response=}")
         response.raise_for_status()
-        ret_list = response.json()["phone_data"]
+        data = response.json()["phone_data"]
 
-    for e in ret_list:
+    for e in data:
         e["data"]["write_ts"] = e["metadata"]["write_ts"]
 
-    print(f"Found {len(ret_list)} entries")
-    return ret_list
+    # dump all API calls to JSON files
+    spec_id_str = "all" if not SPEC_ID else SPEC_ID.replace("_", "-")
+    key_str = key.replace("/", "~").replace("_", "-")
+    user_str = user.replace("@", "#").replace(".", "^")
+    out_file = f"data_{spec_id_str}_{key_str}_{user_str}_{int(start_ts)}_{int(end_ts)}.json"
+
+    with open(out_file, "w") as f:
+            json.dump(data, f, indent=4)
+
+    print(f"Found {len(data)} entries")
+    return data
+
+
+def get_specs():
+    specs = retrieve_data_from_server("config/evaluation_spec", SPEC_USER, 0, arrow.get().timestamp)
+    
+    # filter by spec_id if specified
+    if SPEC_ID:
+        specs = [s for s in specs if s["data"]["label"]["id"] == SPEC_ID]
+
+    # remove duplicate specs based on most recently written spec
+    s_dict = dict()
+    for s in specs:
+        s_id = s["data"]["label"]["id"]
+        if (s_id not in s_dict) or ((s_id in s_dict) and (s["metadata"]["write_ts"] > s_dict[s_id]["metadata"]["write_ts"])):
+            s_dict[s_id] = s
+
+    # populate Spec objects (which consist of spec details) for each unique spec
+    specs = [Spec(s) for s in s_dict.values()]
+    
+    return specs
+
+
+def fill_transitions(specs):
+    pv_maps = dict.fromkeys(specs)
+    for spec in specs:
+        pv_maps[spec] = copy.deepcopy(spec.phones)
+        for phone_os, phone_map in pv_maps[spec].items():
+            for phone_label in phone_map:
+                phone_map[phone_label] = {
+                    "role": phone_map[phone_label],
+                    "transitions": retrieve_data_from_server("manual/evaluation_transition", phone_label, spec.start_ts, spec.end_ts)
+                }
+
+    return pv_maps
+
+
+def build_phone_view_map():
+    # get specified spec, or get all specs if spec isn't specified
+    specs = get_specs()
+
+    # fill transitions
+    pv_maps = fill_transitions(specs)
 
 
 def parse_args():
@@ -71,47 +126,16 @@ def parse_args():
     return parser.parse_args()
 
 
-def main():
+if __name__ == "__main__":
     args = parse_args()
+    
+    # set script-level variables
+    DATASTORE_URL = args.datastore_url
+    SPEC_ID = args.spec_id
 
     # handle case where --key, etc are specified
     if "--key" in sys.argv:
-        data = retrieve_data_from_server(
-            args.datastore_url,
-            args.key,
-            args.user,
-            args.start_ts,
-            args.end_ts)
-
-        spec_id_str = "all" if args.spec_id is None else args.spec_id
-        key_str = args.key.replace("/", "~").replace("_", "-")
-        out_file = f"data_{spec_id_str}_{key_str}_{int(args.start_ts)}_{int(args.end_ts)}.json"
-
-        with open(out_file, "w") as f:
-            json.dump(data, f, indent=4)
-        return
-    
-    # otherwise, build up phone view map
-    ## 1) get specified spec, or get all specs if spec isn't specified
-    specs = retrieve_data_from_server(
-        args.datastore_url,
-        "config/evaluation_spec",
-        DEFAULT_USER,
-        0,
-        arrow.get().timestamp)
-    if args.spec_id:
-        specs = [s for s in specs if s["data"]["label"]["id"] == args.spec_id]
-
-    # remove duplicate specs based on most recently written spec
-    s_dict = dict()
-    for s in specs:
-        spec_id = s["data"]["label"]["id"]
-        if (spec_id not in s_dict) or ((spec_id in s_dict) and (s["metadata"]["write_ts"] > s_dict[spec_id]["metadata"]["write_ts"])):
-            s_dict[spec_id] = s
-
-    ## 2) populate Spec objects (which consist of spec details) for each unique spec
-    specs = [Spec(s) for s in s_dict.values()]
-    print(len(specs))
-
-if __name__ == "__main__":
-    main()
+        retrieve_data_from_server(args.key, args.user, args.start_ts, args.end_ts)
+    else:
+        # run data dumping pipeline
+        build_phone_view_map()
