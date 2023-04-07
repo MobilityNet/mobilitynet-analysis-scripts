cfb9cb2ba99acb09292aa2c4ddfa9f34c700d2a4
K. Shankari <shankari@eecs.berkeley.edu>
2023-04-06 17:06:57 -0700

    Init the shell before activating the environment
    Testing done:
    
    Before this change
    
    ```
    CommandNotFoundError: Your shell has not been properly configured to use 'conda activate'.
    To initialize your shell, run
    
        $ conda init <SHELL_NAME>
    ```

--- setup/setup.sh
+++ setup/setup.sh
@@ -11,4 +11,5 @@ source setup/checks/check_for_conda.sh
 
 conda create -n emissioneval
 conda env update --name emissioneval --file setup/environment.yml
+conda init bash
 conda activate emissioneval
